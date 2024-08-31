77e79aa7917937c5944d903b96521583570d817c
EnzoBeltran <egbeltran01@gmail.com>
2024-08-31 01:03:38 -0300

    E2 - Nunba finalizada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pizzas = [
   {
     id: 1,
-    nombre: "pizza de Muzzarella",
+    nombre: "Pizza de Muzzarella",
     precio: 500,
     ingredientes: [
       "Muzzarella",
@@ -11,7 +11,7 @@ const pizzas = [
 
   {
     id: 2,
-    nombre: "pizza de Cebolla",
+    nombre: "Pizza de Cebolla",
     precio: 1500,
     ingredientes: [
       "Muzzarella",
@@ -21,7 +21,7 @@ const pizzas = [
 
   {
     id: 3,
-    nombre: "pizza Napolitana",
+    nombre: "Pizza Napolitana",
     precio: 1350,
     ingredientes: [
       "Muzzarella",
@@ -32,7 +32,7 @@ const pizzas = [
 
   {
     id: 4,
-    nombre: "pizza 4 Quesos",
+    nombre: "Pizza 4 Quesos",
     precio: 1380,
     ingredientes: [
       "Muzzarella",
@@ -45,7 +45,7 @@ const pizzas = [
 
   {
     id: 5,
-    nombre: "pizza Especial",
+    nombre: "Pizza Especial",
     precio: 1000,
     ingredientes: [
       "Muzzarella",
@@ -57,7 +57,7 @@ const pizzas = [
 
   {
     id: 6,
-    nombre: "pizza con Anana",
+    nombre: "Pizza con Anana",
     precio: 600,
     ingredientes: [
       "Muzzarella",
@@ -71,20 +71,31 @@ const pizzas = [
 //const esImpar = (numero) => numero % 2 !== 0;
 //const pizzasConIdImpar = pizzas.filter(pizzeta => esImpar(pizzeta.id));
 //console.log(pizzasConIdImpar);
+//--------------------------------------------------------------------
 
 //b) Responder: ¿Hay alguna pizza que valga menos de $600 ?
 
-const NumMenor600 = (numero) => numero < 600;
+//const NumMenor600 = (numero) => numero < 600;
 
-const PizzaMenor600 = pizzas.some(barata => NumMenor600(barata.precio));
-console.log(PizzaMenor600);
+//const PizzaMenor600 = pizzas.some(barata => NumMenor600(barata.precio));
+//console.log(PizzaMenor600);
 
+//---------------------------------------------------------------------
 
+//c) El nombre de cada pizza con su respectivo precio
 
+//pizzas.forEach(pizza =>{
+  //console.log(`Pi ${pizza.nombre}, Precio: $${pizza.precio}`);
+//});
 
-//c) El nombre de cada pizza con su respectivo precio.
+//-----------------------------------------------------------
+
 //d) Todos los ingredientes de cada pizza(En cada iteración
 //imprimir los ingredientes de la pizza que se esta recorriendo)
 //.Ayuda: van a tener que realizar dos recorridos, ya que cada
 // pizza del array de pizzas tiene una propiedad "ingredientes"
 //cuyo valor es un array con ingredientes.
+
+//pizzas.forEach(pizza =>{
+  //console.log(`${pizza.nombre}, ingredientes: ${pizza.ingredientes}`);
+//});
